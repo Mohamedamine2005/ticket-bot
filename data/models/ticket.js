521d52cd2c27192390b2abf0c27fd6b0034c4535
Mohamedamine2005 <47900954+Mohamedamine2005@users.noreply.github.com>
2021-03-16 17:51:11 +0100

    Add files via upload

---
+++ data/models/ticket.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const ticketModel = mongoose.model(
+  "ticket",
+  new mongoose.Schema({
+    guild: String,
+    msg: String,
+    owner: String,
+    channelID: String,
+    status: { type: Boolean, default: true },
+    reason: String,
+  })
+);
+module.exports = ticketModel;
